a923b4c1ca1973350dd90ec198a7ebf452b46558
Anthony Restaino <anthonyrestaino11@gmail.com>
2014-07-09 17:25:13 -0400

    Updated homepage, bookmarks, history pages so titles are translatable

--- src/acr/browser/thunder/BrowserApp.java
+++ src/acr/browser/thunder/BrowserApp.java
@@ -1,7 +1,17 @@
 package acr.browser.thunder;
 
 import android.app.Application;
+import android.content.Context;
 
 public class BrowserApp extends Application {
+	private static Context context;
 
+	public void onCreate() {
+		super.onCreate();
+		context = getApplicationContext();
+	}
+
+	public static Context getAppContext() {
+		return context;
+	}
 }
