9be92a46da696a1ce33655a46e3ebaf6db94aeb6
Anthony Restaino <anthonyrestaino11@gmail.com>
2014-07-10 21:43:58 -0400

    Centralized Browser Activity for easier maintenance of Incognito and Normal modes

--- src/acr/browser/thunder/BrowserActivity.java
+++ src/acr/browser/thunder/BrowserActivity.java
@@ -158,9 +158,7 @@ public class BrowserActivity extends Activity implements BrowserController {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
