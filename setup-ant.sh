7e5804618b73d46e746a478b2693250c8242b71c
Anthony Restaino <anthonyrestaino11@gmail.com>
2014-07-22 18:36:08 -0400

    Update setup-ant.sh

--- setup-ant.sh
+++ setup-ant.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-target="android-19"
+target="android-20"
 
+# Travis doesn't support android-20 yet
+if [ -n "$TRAVIS" ]; then
+    target="android-19";
+fi
+
+# Update ant setup in project and all sub-projects
 for f in `find external/ -name project.properties`; do
-projectdir=`dirname $f`
+    projectdir=`dirname $f`
     echo "Updating ant setup in $projectdir:"
     android update lib-project -p $projectdir -t $target
 done
-android update project -p . --subprojects -t $target --name Thunder
+android update project -p . --subprojects -t $target --name Lightning
+
 
 cp libs/android-support-v4.jar external/netcipher/libnetcipher/libs/android-support-v4.jar
 
